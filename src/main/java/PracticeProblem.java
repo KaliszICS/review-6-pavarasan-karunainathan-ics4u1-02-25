2c73fd41977fb4e3e228a74cd4c024ddf522b97e
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-02-11 14:37:23 +0000

    Problems Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,39 @@
 public class PracticeProblem {
-
-	public static void main(String args[]) {
-
+	public static int sum(int num1, int num2){
+		return num1 + num2;
 	}
 
+	public static int difference(int num1, int num2){
+		return num1 - num2;
+	}
+
+	public static double product(double num1, double num2){
+		return num1 * num2;
+	}
+	
+	public static String removeFirst(String str){
+		return str.substring(1);
+	}
+
+	public static int combinedLength(String str1, String str2){
+		return str1.length() + str2.length();
+	}
+
+	public static boolean isEven(int num){
+		return (num & 1) == 0;
+	}
+
+	public static boolean isOdd(int num){
+		return (num & 1) == 1;
+	}
+
+	public static boolean isPositive(int num){
+		return num > 0;
+	}
+
+	public static boolean isNegative(int num){
+		return num < 0;
+	}
+
+	public static void main(String args[]){}
 }
